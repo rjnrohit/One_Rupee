57849296d4c615a5ae89bcae89f6fcf196577026
sbhagat1729 <1729sb@gmail.com>
2019-06-22 05:55:32 +0530

    update payment history table

--- one_rupee_frontend/src/components/pages/donations/Donations.js
+++ one_rupee_frontend/src/components/pages/donations/Donations.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Table from './Table.js'
 import './Donations.css'
 
+const sampleHistory = {
+    date: '21/6/19' , 
+    time: '21:00' ,
+    Name: 'abc' , 
+    Amount: '1,00,00,000' ,
+}
+
 class Donations extends React.Component{
     constructor() {
         super()
         this.state = {
-            payments: [
-                { date: '', Name: '', Amount: ''},
-                { date: '', Name: '', Amount: ''},
-                { date: '', Name: '', Amount: ''},
-                { date: '', Name: '', Amount: ''},
-            ]
+            payments: [{date: '', time: '', Name: '', Amount: ''}]
         }
     }
+
+    componentDidMount(){
+        // Fetch 
+        this.setState({payments:[sampleHistory, sampleHistory, sampleHistory, sampleHistory]})
+    }
+    
     render(){
         return(
             <div>
