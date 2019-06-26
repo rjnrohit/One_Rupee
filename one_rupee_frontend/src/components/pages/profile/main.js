82e43ec8ce548e41e8661344f3b5a01adecbe480
sbhagat1729 <1729sb@gmail.com>
2019-06-26 06:27:25 +0530

    minor changes

--- one_rupee_frontend/src/components/pages/profile/main.js
+++ one_rupee_frontend/src/components/pages/profile/main.js
@@ -26,14 +26,14 @@ class Main extends React.Component
             height="200"
             width="200"
             />  
-            <h1>Hey,{this.state.name}</h1>
+            <h1>Hey, {this.state.name}</h1>
             </div>
             <div id="card">
                 <h2><b>Name :</b> {this.state.name}</h2>
                 <h2><b>Email :</b> {this.state.email}</h2>
                 <h2><strong>Mobile no. :</strong> {this.state.mobNo}</h2>
                 <h3>You have made {this.state.numDonations} donations so far...</h3>
-                <h4><a>Click here</a> to check your donations.</h4>
+                <h4>Click <a href="./">here</a> to check your donations.</h4>
                 {/*to be done after router work*/}
             </div>
             
--- one_rupee_frontend/src/components/pages/profile/profile.jsx
+++ one_rupee_frontend/src/components/pages/profile/profile.jsx
@@ -1,16 +1,10 @@
-import React, { Component } from "react";
-import "../landing/css/footer.css"
-import Footer from "../landing/js/footer.js"
+import React from "react";
 import Main from "./main.js"
 
 function Profile(){
     return (
-      <div >
         <Main/>
-        <Footer/>
-      </div>
     );
   }
-
 
 export default Profile;
