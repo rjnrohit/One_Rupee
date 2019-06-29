3a330621d6571d5921ab858e1ce322c097af4fdc
shayanuh <50916646+shayanuh@users.noreply.github.com>
2019-06-30 01:45:56 +0530

    Update main.jsx

--- one_rupee_frontend/src/components/main.jsx
+++ one_rupee_frontend/src/components/main.jsx
@@ -8,7 +8,7 @@ import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
 const Main = props => {
   return (
     <Router>
-      
+      <Switch>
         <div
         className={
           "main-wrapper" +
@@ -17,11 +17,11 @@ const Main = props => {
       >
     <Route path ="/" exact component = {Landing}/>
     <Route path ="/history" component={History}/>
-    
     <Route path ="/profile" component = {Profile}/>
     <Route path ="/feed" component={Feed}/>
     <Route path ="/request" component={Request}/>
       </div>
+      </Switch>
     </Router>
   );
 };
