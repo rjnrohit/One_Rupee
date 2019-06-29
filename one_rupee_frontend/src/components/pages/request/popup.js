3698c92650c0433da444f3ef50bcc84c76122b8c
shayanuh <shayanuh@iitk.ac.in>
2019-06-30 00:48:50 +0530

    add css in request form

--- one_rupee_frontend/src/components/pages/request/popup.js
+++ one_rupee_frontend/src/components/pages/request/popup.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./request.css"
+// import { border } from "@material-ui/system";
 
 class Popup extends React.Component
 {
@@ -8,21 +9,23 @@ class Popup extends React.Component
         return(
             <div className='popup'>
                 <div className='popup_inner'>
-                    <button onClick={this.props.closePopup} id="closebtn"> Cancel</button>
-                    Register your new project request here:
+                    
+                    <b id ="a1">Register your new project request here:</b>
                     <form>
-                        <label><b> Project Name :</b></label><input type="text" placeholder ="Enter the name of your project" ></input>
-                        <label><b>Category of your Project:</b></label>
+                        <label><h2><b> Project Name :</b></h2></label><input type="text" placeholder ="Enter the name of your project" ></input>
+                        <label><h2><b>Category of your Project:</b></h2></label>
                         <select name="cars">
                             <option value= "Educational">Educational</option>
                             <option value= "Health">Health</option>
                             <option value= "Charity">Charity</option>
                             <option value= "Other">Other</option>
                         </select>
-                        <label><b>Description</b></label>
+                        <label><h2><b>Description</b></h2></label>
                         <input type="textarea"/>
-                        <label><b> Expected Amount :</b></label><input type="text" ></input>
+                        <label><h2><b> Expected Amount :</b></h2></label><input type="text" ></input>
+                        <br/><br/>
                         <button type = "Submit">Submit</button> 
+                        <button onClick={this.props.closePopup} id="closebtn"> Cancel</button>
                     </form>
                 </div>
             </div>
--- one_rupee_frontend/src/components/pages/request/request.js
+++ one_rupee_frontend/src/components/pages/request/request.js
@@ -3,6 +3,7 @@ import Popup from "./popup.js"
 import "./request.css"
 import ReqPost from "./reqPost"
 
+
 const samplePost = {
     ngo: {
       title: "[TITLE]",
@@ -52,15 +53,18 @@ class Request extends React.Component
     {
         return(
             <div>
+                <div><h1 style={{textAlign:"center"}}>My Requests</h1></div>
                 <div className="feed-container">
                     {this.state.posts.map((post, index) => (
                     <ReqPost post={post} key={index}/>
                     ))}
                 </div>
-                <button  onClick={this.togglePopup.bind(this)}>
-                    Request for Donation !
-                </button>
-                {this.state.showPopup ? <Popup closePopup = {this.togglePopup.bind(this)}/>:null}
+                <div id="req">
+                    <button  onClick={this.togglePopup.bind(this)}>
+                        Request for Donation !
+                    </button>
+                    {this.state.showPopup ? <Popup closePopup = {this.togglePopup.bind(this)}/>:null}
+                </div>
             </div>
         )
     }
