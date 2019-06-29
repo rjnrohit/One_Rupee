3c36b97fc4defd6f6decff7bc9917d9166aff025
shayanuh <shayanuh@iitk.ac.in>
2019-06-29 19:10:42 +0530

    minor change in css

---
+++ one_rupee_frontend/src/components/pages/leaderboard/leaderboard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Table from "./table.js"
+import Title from "./title.js"
+function LeaderBoard()
+{
+    return(
+        <div>
+            <Title/>
+            <Table/>
+        </div>
+
+    )
+}
+export default LeaderBoard;
---
+++ one_rupee_frontend/src/components/pages/leaderboard/table.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+class Table extends React.Component
+{
+    constructor()
+    {
+        super()
+        this.state={
+
+        }
+    }
+    render()
+    {
+        return(
+            <table>
+                
+            </table>
+        )
+    }
+}
+export default Table;
