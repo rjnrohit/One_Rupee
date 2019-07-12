b4b21e5252ac55b51f87d89184e326fe4720bc35
shayanuh <shayanuh@iitk.ac.in>
2019-07-13 00:29:28 +0530

    request page done

--- one_rupee_frontend/src/components/pages/request/request.js
+++ one_rupee_frontend/src/components/pages/request/request.js
@@ -1,7 +1,61 @@
 import React from "react"
 import Popup from "./popup.js"
 import ReqPost from "./reqPost"
+import axios from "axios"
 
+const categories = [
+  'AGE CARE',
+  'AGRICULTURE',
+  'ANIMAL WELFARE',
+  'ART AND CRAFT',
+  'CHILD EDUCATION',
+  'CITIES/URBAN DEVELOPMENT',
+  'COMMUNITY DEVELOPMENT',
+  'CULTURE AND HERITAGE',
+  'DISABILITY',
+  'DISASTER MANAGEMENT',
+  'DRINKING WATER',
+  'EDUCATION',
+  'ENVIRONMENTAL ISSUES',
+  'HEALTH AND HYGIENE',
+  'HIV/AIDS',
+  'HOUSING AND SLUMS',
+  'POPULATION',
+  'POVERTY REMOVAL',
+  'RURAL DEVELOPMENT',
+  'SCIENCE AND TECHNOLOGY DEVELOPMENT',
+  'TRIBAL PEOPLE',
+  'WASTE MANAGEMENT',
+  'WOMEN',
+  'OTHERS'
+]
+
+const shortCategories = [
+  "AGC" ,
+  "AGR" ,
+  "AW",
+  "ANC" ,
+  "CE" ,
+  "CUD",
+  "CD",
+  "CNH" ,
+  "D",
+  "DM",
+  "DW",
+  "EDU",
+  "ENVI",
+  "HNH",
+  "HA" ,
+  "HS",
+  "P",
+  "PR",
+  "RD",
+  "STD",
+  "TP",
+  "WM",
+  "W",
+  "O"
+]
 
 const samplePost = {
     ngo: {
@@ -22,22 +76,65 @@ const samplePost = {
 
 class Request extends React.Component
 {
-    constructor()
+    constructor(props)
     {
-        super()
+        super(props)
         this.state={
             posts: [],
+            username:"",
+            token:this.props.token,
+            pk:this.props.pk,
         };
     }
 
     componentDidMount() {
         // fetch
+        const url = "http://localhost:8000/ngo/view-profile/" + this.props.pk + "/"
+    axios({url:url,
+      headers:{Authorization:"Token "+this.props.token}
+    }).then(res=>{
+      this.setState({username:res.data.Ngo.username})
+      const url2="http://localhost:8000/cards/card-list-view/"+this.state.username +"/"
+      axios({url:url2,
+      headers:{Authorization:"Token "+this.state.token},
+      method:"get",
+      transformResponse:[res=>{
+        // console.log(res)
+        res=JSON.parse(res)
+        res.results.map(info=>{
+        const idx = shortCategories.indexOf(info.category)
+        const newPost = {
+          ngo: {
+            title: info.title,
+            summary: info.shortDescription,
+            category: categories[idx],
+            description: info.longDescription,
+            required: info.amount_requested,
+            obtained: 0,
+          },
+          type: {
+            name: info.ngo.ngo_name,
+            image: ""
+          }
+        }
+        this.setState(prevState=>({
+          posts: [...prevState.posts, newPost]
+        }))}
+      )
+      return res;
+      }]
+    }).then(res=>
+      {
+        console.log("success")
+      }).catch(err=>
+        {
+          console.log(err)
+        })
+    }).catch(err=>{
+      console.log(err)
+    })
         this.setState({
-          posts: [
-            samplePost,
-            samplePost,
-            samplePost,
-          ]
+          posts: []
         });
     }
 
