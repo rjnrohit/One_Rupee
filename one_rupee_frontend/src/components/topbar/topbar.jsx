f1198f9697f9b4e4da2f948bace90c3d31d28f36
sbhagat1729 <1729sb@gmail.com>
2019-07-13 06:47:52 +0530

    remove sidebar and topbar in landing

--- one_rupee_frontend/src/components/topbar/topbar.jsx
+++ one_rupee_frontend/src/components/topbar/topbar.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import SidebarToggle from "../sidebar/sidebarToggle";
 
 const TopBar = props => {
+  if(props.token==="") return(
+    <nav className="topbar-container">
+    </nav>
+  )
   return (
     <nav className="topbar-container">
       <SidebarToggle onSidebarToggle={props.onSidebarToggle} />
